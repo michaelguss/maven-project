7410c9350f010ad0c3495684e047f3cdceded992
Michael Guß <michaelguss@Michaels-MBP-2.office.adorsys.de>
2017-04-05 14:28:27 +0200

    checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
 /**
- * This MAKES THE GREET.
+ * @param only a string
+ * @return the hello string
  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
